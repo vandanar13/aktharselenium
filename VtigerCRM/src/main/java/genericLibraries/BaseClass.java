cb977c1603d440a4e91abeadf15c95f09bcdcd23
akthar <aktharniyaz90427@gmail.com>
2023-07-10 08:44:28 +0530

    m

--- VtigerCRM/src/main/java/genericLibraries/BaseClass.java
+++ VtigerCRM/src/main/java/genericLibraries/BaseClass.java
@@ -6,10 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
@@ -26,13 +23,13 @@ import pomClass.OrgnizationPage;
 import pomClass.SignOuts;
 
 public class BaseClass {
-	
+
 	protected ExcelUtility excel;
 	protected WebDriverUtility web;
 	protected static WebDriver driver;
 	protected propertiesUtility ppt;
 	protected ExtentReports report;
-	
+
 	protected HomePage home;
 	protected OrgnizationPage organization;
 	protected SignOuts signOut;
@@ -40,41 +37,35 @@ public class BaseClass {
 	protected LeadPage lead;
 	protected CreateOrganizationpage createOrganization;
 	protected LoginPage login;
-	
+
 	protected CalendarPage calender;
-	
-	
-	@BeforeClass(alwaysRun=true)
+
+	@BeforeClass(alwaysRun = true)
 	public void LauchingBrowser() throws IOException {
 		ppt = new propertiesUtility();
 		excel = new ExcelUtility();
 		web = new WebDriverUtility();
 		report = new ExtentReports();
-		
-		
-		if(ppt.readDataFromProperties("browser").equalsIgnoreCase("chrome")) {
+
+		if (ppt.readDataFromProperties("browser").equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-			}
-		else {
+		} else {
 			WebDriverManager.chromedriver().setup();
 			driver = new FirefoxDriver();
 		}
-		
+
 		driver.manage().window().maximize();
-		
+
 		driver.get(ppt.readDataFromProperties("url"));
-		
+
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		
-		
+
 	}
-	
-	
+
 	@BeforeMethod(alwaysRun = true)
 	public void navigatingToApplication() throws IOException {
-		
+
 		calender = new CalendarPage(driver);
 		login = new LoginPage(driver);
 		home = new HomePage(driver);
@@ -83,22 +74,23 @@ public class BaseClass {
 		contact = new Contactpage(driver);
 		lead = new LeadPage(driver);
 		createOrganization = new CreateOrganizationpage(driver);
-		
+
 		login.getUserNameTF().sendKeys(ppt.readDataFromProperties("username"));
 		login.getPasswordTF().sendKeys(ppt.readDataFromProperties("password"));
 		login.getLoginButton().click();
-		
-		}
-	
+
+	}
+
 	@AfterClass
 	public void tearDownMethod() {
-		/*if(result.getStatus()==result.FAILURE) {
-			String path = utility.takingScreenshot(result.getTestName());
-		}*/
-		
+		/*
+		 * if(result.getStatus()==result.FAILURE) { String path =
+		 * utility.takingScreenshot(result.getTestName()); }
+		 */
+
 		web.mouseHoverOnElement(signOut.getAdminIcon());
 		signOut.getSignOutButton().click();
-		
+
 		driver.quit();
 	}
 }
